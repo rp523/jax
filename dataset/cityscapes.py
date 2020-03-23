e991603c83daa9c4fd69c3300e89dbbcd57149ff
Yusuke <goto.yusuke@subaru.co.jp>
2020-03-23 21:48:19 +0900

    batch-size

--- dataset/cityscapes.py
+++ dataset/cityscapes.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 from PIL import Image
 import json
+from jax.interpreters.batching import batch
 
 '''
 [Labels]
@@ -120,39 +121,55 @@ class CityScapes:
                     label_dict[obj_label] = np.append(label_dict[obj_label], rect, axis = 0)
         return label_dict
     
-    def make_generator(self, train_type, label_txt_list, seed):
+    def make_generator(self, train_type, label_txt_list, batch_size, seed):
         np.random.seed(seed)
         train_type_dict = self.__all[train_type]
         key_list = list(train_type_dict.keys())
         n_data = len(key_list)
+        
+        images = np.empty(0)
+        labels = []
         while True:
-            i = np.random.randint(n_data)
-            key = key_list[i]
-            tgt = train_type_dict[key]
-            assert("left" in tgt.keys())
-            if len(label_txt_list) > 0:
-                if "json" in tgt.keys():
-                    label_info = self.__json_to_rects(tgt["json"], label_txt_list)
+            for b in range(batch_size):
+                i = np.random.randint(n_data)
+                key = key_list[i]
+                tgt = train_type_dict[key]
+                assert("left" in tgt.keys())
+                if len(label_txt_list) > 0:
+                    if "json" in tgt.keys():
+                        label_info = self.__json_to_rects(tgt["json"], label_txt_list)
+                    else:
+                        label_info = {}
+                        for label in label_txt_list:
+                            label_info[label] = []
+    
+                left_path = tgt["left"]
+                left_pil = Image.open(left_path)
+                left_pil = left_pil.resize((self.__img_w, self.__img_h))
+                left_arr = np.asarray(left_pil)
+                left_arr = left_arr.reshape((1,
+                                             left_arr.shape[0],
+                                             left_arr.shape[1],
+                                             left_arr.shape[2]))
+                if images.size == 0:
+                    images = left_arr
                 else:
-                    label_info = {}
-                    for label in label_txt_list:
-                        label_info[label] = []
-
-            left_path = tgt["left"]
-            left_pil = Image.open(left_path)
-            left_pil = left_pil.resize((self.__img_w, self.__img_h))
-            left_arr = np.asarray(left_pil)
-            
-            yield left_arr, label_info
+                    images = np.append(images, left_arr, axis = 0)
+                labels.append(label_info)
+                
+            yield images, labels
             
 def visualize(data_path, dst_dir_path):
     from PIL import ImageDraw, ImageFont
     cityscapes = CityScapes(data_path, 256, 512)
+    batch_size = 30
     gen = cityscapes.make_generator("train",
                                     label_txt_list = ["car", "person"],
+                                    batch_size = batch_size,
                                     seed = 0)
-    for i in range(300):
-        left_arr, label_dict = next(gen)
+    images, labels = next(gen)
+    for b in range(batch_size):
+        left_arr, label_dict = images[b], labels[b]
         pil = Image.fromarray(left_arr)
         dr = ImageDraw.Draw(pil)
         for label_name, rect_arr in label_dict.items():
@@ -165,7 +182,7 @@ def visualize(data_path, dst_dir_path):
                 yc = (y0 + y1) // 2
                 dr.rectangle((x0, y0, x1, y1), width = 3, outline = (255, 0, 0))
                 dr.text((xc, yc), label_name, fill = (255, 0, 0))
-        dst_path = os.path.join(dst_dir_path, "{}.jpg".format(i))
+        dst_path = os.path.join(dst_dir_path, "{}.jpg".format(b))
         pil.save(dst_path)
         print(dst_path)
 
