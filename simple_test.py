414830a09b5ef5ff37a95aeaf5a4dc737556960c
Yusuke <isgsktyktt@gmail.com>
2020-07-04 13:29:13 +0900

    2次元拡張

--- simple_test.py
+++ simple_test.py
@@ -25,20 +25,26 @@ def Swish():
 
 def Net():
     net = net_maker()
-    net.add_layer(serial(Dense(100), Swish()))
+    net.add_layer(serial(Dense(300), Swish()))
     for _ in range(1):
-        net.add_layer(serial(Dense(100), Swish()))
+        net.add_layer(serial(Dense(300), Swish()))
     net.add_layer(Dense(1), name = "out")
     return net.get_jax_model()
 
 def tgt_fun(x):
-    return jnp.cos(x)# + jnp.sin(x.T[1])
+    print(x.shape)
+    y = jnp.cos(x.T[0])#
+    if x.ndim == 2:
+        #y *= jnp.sin(x.T[1])
+        pass
+    return y
 
 def main(is_training):
     LR = 1E-3
     BATCH_SIZE = 32
+    X_DIM = 1
     SAVE_PATH = "simple.bin"
-    broaden_rate = 5
+    broaden_rate = 4
     half = 4 * jnp.pi * broaden_rate
     band = half * 2
 
@@ -50,14 +56,16 @@ def main(is_training):
         init_params = pickle.load(open(SAVE_PATH, "rb"))
         print("LOADED INIT PARAMS")
     else:
-        _, init_params = init_fun(rng, (BATCH_SIZE, 1))
+        _, init_params = init_fun(rng, (BATCH_SIZE, X_DIM))
     opt_state = opt_init(init_params)
 
     def loss(params, x, y):
         p = apply_fun(params, x)["out"]
+        y = y.reshape((-1, 1))
         assert(p.shape == y.shape)
         return ((p - y) ** 2).sum() / x.shape[0]
 
+    #@jax.jit
     def update(i, opt_state, x, y):
         params = get_params(opt_state)
         loss_val, grad_val = jax.value_and_grad(loss)(params, x, y)
@@ -67,7 +75,7 @@ def main(is_training):
     e = 0
     while is_training:
         rng1, rng = jax.random.split(rng)
-        x = jax.random.uniform(rng1, (BATCH_SIZE, 1)) * band - half
+        x = jax.random.uniform(rng1, (BATCH_SIZE, X_DIM)) * band - half
         y = tgt_fun(x / broaden_rate)
         loss_val, opt_state = update(e, opt_state, x, y)
         t1 = time.time()
@@ -77,13 +85,24 @@ def main(is_training):
             pickle.dump(get_params(opt_state), open(SAVE_PATH, "wb"))
         e += 1
 
-    point_num = 100
-    x = jnp.linspace(-half, half, point_num).reshape((-1, 1))
-    y = tgt_fun(x / broaden_rate)
-    p = apply_fun(get_params(opt_state), x)["out"]
-    plt.clf()
-    plt.plot(x, y)
-    plt.plot(x, p.flatten())
+    bin_num = 100
+    plot_band = half * 1
+    x = jnp.linspace(-plot_band, plot_band, bin_num)
+    x = jnp.tile(x.reshape(1, -1), (bin_num, 1))
+    y = jnp.linspace(-plot_band, plot_band, bin_num)
+    y = jnp.tile(y.reshape(-1, 1), (1, bin_num))
+    data = jnp.append(x.reshape(-1, 1), y.reshape(-1, 1), axis = 1)
+    assert(data.shape == (bin_num * bin_num, 2))
+    #minus_E = apply_fun(init_params, data / plot_band)["out"]
+    minus_E = tgt_fun(data / plot_band)
+    unnorm_log_q = minus_E
+    unnorm_log_q = unnorm_log_q.reshape((bin_num, bin_num))
+    print(unnorm_log_q.min(), unnorm_log_q.max())
+    X = jnp.linspace(-plot_band, plot_band, bin_num)
+    Y = jnp.linspace(-plot_band, plot_band, bin_num)
+    X, Y = jnp.meshgrid(X, Y)
+    plt.pcolor(X, Y, unnorm_log_q)
+    plt.colorbar()
     plt.show()
 
 if __name__ == "__main__":
