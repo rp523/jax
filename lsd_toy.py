0454a2ef0f8d7e0aa28a1e2cc91c2fbd6394e0d6
Yusuke <isgsktyktt@gmail.com>
2020-07-14 21:43:26 +0900

    できるところまでファイル分割

--- lsd_toy.py
+++ lsd_toy.py
@@ -4,7 +4,8 @@ import jax
 from jax.experimental.stax import serial, Dense, elementwise, FanOut, FanInSum, parallel, Identity
 import jax.experimental.optimizers as optimizers
 import jax.numpy as jnp
-from ebm.sampler import Sampler
+from ebm.toy_sampler import Sampler
+from ebm.lsd import LSD_Learner
 from model.maker.model_maker import net_maker
 
 SEED = 0
@@ -42,29 +43,6 @@ def mlp(out_ch):
         net.add_layer(serial(SkipDense(300), Swish()))
     net.add_layer(Dense(out_ch), name = "out")
     return net.get_jax_model()
-
-def gaussian_net(base_net, init_mu, init_sigma):
-    base_init_fun, base_apply_fun = base_net
-    def init_fun(rng, input_shape):
-        rng_base, rng_mu, rng_sigma = jax.random.split(rng, 3)
-        _, base_params = base_init_fun(rng_base, input_shape)
-        mu_shape = tuple(input_shape[1:])
-        mu = jax.nn.initializers.ones(rng_mu, mu_shape) * init_mu
-        log_sigma = jnp.log(jax.nn.initializers.ones(rng_sigma, (1,)) * init_sigma)
-        params = list(base_params) + [(mu, log_sigma)]
-        output_shape = input_shape
-        return output_shape, params
-    def apply_fun(params, inputs, **kwargs):
-        base_params = params[:-1]
-        mu, log_sigma = params[-1]
-        mu = mu.reshape(tuple([1] + list(mu.shape)))
-        sigma = jnp.exp(log_sigma)
-        base_output = base_apply_fun(base_params, inputs)["out"]
-        log_gauss = - ((inputs - mu) ** 2).sum(axis = -1) / (2 * sigma ** 2)
-        log_gauss = log_gauss.reshape((base_output.shape[0], -1))
-        ret = base_output + log_gauss
-        return ret
-    return init_fun, apply_fun
 
 def save_map(   q_apply_fun, q_params,
                 f_apply_fun, f_params,
@@ -112,68 +90,6 @@ def get_scale(sampler):
         x = jnp.append(x, sampler.sample(), axis = 0)
     return x.mean(), x.std()
 
-class LSD_Learner:
-    @staticmethod
-    def calc_sq_batch(q_params, x_batch, arg_q_apply_fun):
-        def logq_sum(q_params, x_batch):
-            logq_batch = arg_q_apply_fun(q_params, x_batch)
-            return logq_batch.sum()
-        sq_batch = jax.grad(logq_sum, argnums = 1)(q_params, x_batch) # ▽x(Log(q))
-        return sq_batch
-    @staticmethod
-    def calc_efficient_trace(f_params, x_batch, arg_f_apply_fun, rng):
-        eps = jax.random.normal(rng, x_batch.shape)
-        fx, vjp_fun = jax.vjp(arg_f_apply_fun, f_params, x_batch)
-        dp, dx = vjp_fun(eps)
-        return (dx * eps).sum(axis = -1), fx
-    @staticmethod
-    def calc_exact_trace(f_params, x_batch, arg_f_apply_fun):
-        trace_batch = jnp.zeros((BATCH_SIZE,))
-        def f_apply_fun_dim(f_params, x_batch, idx):
-            return arg_f_apply_fun(f_params, x_batch)[:, idx].sum()
-        for d in range(X_DIM):
-            trace_comp = jax.grad(f_apply_fun_dim, argnums = 1)(f_params, x_batch, d)[:, d]
-            trace_batch += trace_comp
-        return trace_batch
-    @staticmethod
-    def calc_loss_metrics(  q_params, f_params, x_batch,
-                            arg_q_apply_fun, arg_f_apply_fun, rng):
-        tr_dfdx_batch, fx_batch = LSD_Learner.calc_efficient_trace(f_params, x_batch, arg_f_apply_fun, rng)
-        sq_batch = LSD_Learner.calc_sq_batch(q_params, x_batch, arg_q_apply_fun)
-        sq_fx_batch = (sq_batch * fx_batch).sum(axis = -1)
-        lsd = (sq_fx_batch + tr_dfdx_batch).mean()
-        f_norm = (fx_batch * fx_batch).sum(axis = -1).mean()
-        return lsd, f_norm
-    @staticmethod
-    def q_loss( q_params, f_params, x_batch,
-                arg_q_apply_fun, arg_f_apply_fun, rng):
-        lsd, _ =  LSD_Learner.calc_loss_metrics(q_params, f_params, x_batch,
-                                    arg_q_apply_fun, arg_f_apply_fun, rng)
-        loss = lsd + 1E-5 * net_maker.weight_decay(q_params)
-        return loss
-    @staticmethod
-    def f_loss( q_params, f_params, x_batch,
-                arg_q_apply_fun, arg_f_apply_fun, rng):
-        lsd, f_norm =  LSD_Learner.calc_loss_metrics(   q_params, f_params, x_batch,
-                                            arg_q_apply_fun, arg_f_apply_fun, rng)
-        return -lsd + LAMBDA * f_norm
-    @staticmethod
-    def q_update(   t_cnt, q_opt_state, f_opt_state, x_batch,
-                    arg_q_apply_fun, arg_f_apply_fun, arg_q_get_params, arg_f_get_params, arg_q_opt_update, rng):
-        q_params = arg_q_get_params(q_opt_state)
-        f_params = arg_f_get_params(f_opt_state)
-        loss_val, grad_val = jax.value_and_grad(LSD_Learner.q_loss, argnums = 0)(q_params, f_params, x_batch, arg_q_apply_fun, arg_f_apply_fun, rng)
-        q_opt_state = arg_q_opt_update(t_cnt, grad_val, q_opt_state)
-        return q_opt_state, loss_val
-    @staticmethod
-    def f_update(c_cnt, q_opt_state, f_opt_state, x_batch,
-                    arg_q_apply_fun, arg_f_apply_fun, arg_q_get_params, arg_f_get_params, arg_f_opt_update, rng):
-        q_params = arg_q_get_params(q_opt_state)
-        f_params = arg_f_get_params(f_opt_state)
-        loss_val, grad_val = jax.value_and_grad(LSD_Learner.f_loss, argnums = 1)(q_params, f_params, x_batch, arg_q_apply_fun, arg_f_apply_fun, rng)
-        f_opt_state = arg_f_opt_update(c_cnt, grad_val, f_opt_state)
-        return f_opt_state, loss_val
-
 def main():
     _rng = jax.random.PRNGKey(SEED)
     
@@ -181,7 +97,7 @@ def main():
     sampler = Sampler(rng_s, BATCH_SIZE * (C + 1), HALF_BAND)
     mu, sigma = get_scale(sampler)
 
-    q_init_fun, q_apply_fun = gaussian_net(mlp(1), mu, sigma)
+    q_init_fun, q_apply_fun = LSD_Learner.gaussian_net(mlp(1), mu, sigma)
     f_init_fun, f_apply_fun_raw = mlp(2)
     def f_apply_fun(f_params, x):
         return f_apply_fun_raw(f_params, x)["out"]
@@ -202,7 +118,7 @@ def main():
         idx += 1
         t_cnt += 1
         for _ in range(C):
-            f_opt_state, f_loss_val = LSD_Learner.f_update( c_cnt, q_opt_state, f_opt_state, x_batch[idx * BATCH_SIZE : (idx+1) * BATCH_SIZE],
+            f_opt_state, f_loss_val = LSD_Learner.f_update( c_cnt, q_opt_state, f_opt_state, x_batch[idx * BATCH_SIZE : (idx+1) * BATCH_SIZE], LAMBDA,
                                                 q_apply_fun, f_apply_fun, q_get_params, f_get_params, f_opt_update, rngs[idx])
             idx += 1
             c_cnt += 1
