2009a544cbfa6cdeaae63e9acb4363e643898728
rp523 <me@example.com>
2020-08-17 23:47:47 +0900

    Evidential学習を実装

--- dirichlet.py
+++ dirichlet.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 import os, time, pickle
+import numpy as np
 import jax
 import jax.numpy as jnp
 from jax.experimental.stax import serial, parallel, Dense, Tanh, Conv, Flatten, FanOut, FanInSum, Identity
@@ -36,6 +37,8 @@ def nn(class_num):
                     Dense(100), Tanh,
                     Dense(class_num)
                     )
+def digamma(x):
+    return jax.scipy.special.digamma(x)
 
 @hydra.main("dirichlet.yaml")
 def main(cfg):
@@ -45,8 +48,10 @@ def main(cfg):
     lr = cfg.optim.lr
     focal_gamma = cfg.optim.focal_gamma
     epoch_num = cfg.optim.epoch_num
+    burnin_epoch = cfg.optim.burnin_epoch
     log_sec = cfg.optim.log_sec
     weight_name = cfg.optim.weight_name
+    loss_type = cfg.optim.loss_type
 
     init_fun, apply_fun = nn(10)
     rng = jax.random.PRNGKey(seed)
@@ -68,10 +73,37 @@ def main(cfg):
         y_pred = jax.nn.softmax(y_pred)
         ce = (- y * ((1.0 - y_pred) ** focal_gamma) * jnp.log(y_pred + 1E-10)).sum(axis = -1).mean()
         return ce
+    def kl_to_ones(y, alpha):
+        alpha_ = y + (1.0 - y) * alpha
+        s_ = alpha_.sum(axis = -1, keepdims = True)
+        t1 = jax.scipy.special.gammaln(s_).sum(axis = -1)
+        t2 = -(jnp.log(alpha_)).sum(axis = -1)
+        t3 = ((alpha_ - 1.0) * (digamma(alpha_) - digamma(s_))).sum(axis = -1)
+        kl = (t1 + t2 + t3).mean()
+        return kl
+    def dirichlet_l2_loss(params, x, y, prio_weight):
+        y_pred = apply_fun(params, x)
+        alpha = jnp.exp(y_pred)
+        s = alpha.sum(axis = -1, keepdims = True)
+        p = alpha / s
+        loss = ((y - p) ** 2 + p * (1.0 - p) / (s + 1.0)).sum(axis = -1).mean()
+        return loss + prio_weight * kl_to_ones(y, alpha)
+    def dirichlet_ce_loss(params, x, y, prio_weight):
+        y_pred = apply_fun(params, x)
+        alpha = jnp.exp(y_pred)
+        s = alpha.sum(axis = -1, keepdims = True)
+        loss = y * (digamma(s) - digamma(alpha))
+        loss = loss.sum(axis = -1).mean()
+        return loss + prio_weight * kl_to_ones(y, alpha)
     @jax.jit
-    def update(idx, opt_state, x, y):
+    def update(idx, opt_state, x, y, prio_weight):
         params = get_params(opt_state)
-        loss_val, grad_val = jax.value_and_grad(ce_loss)(params, x, y)
+        if loss_type == "dili_ce":
+            loss_val, grad_val = jax.value_and_grad(dirichlet_ce_loss)(params, x, y, prio_weight)
+        elif loss_type == "dili_l2":
+            loss_val, grad_val = jax.value_and_grad(dirichlet_l2_loss)(params, x, y, prio_weight)
+        else:
+            loss_val, grad_val = jax.value_and_grad(ce_loss)(params, x, y)
         opt_state = opt_update(idx, grad_val, opt_state)
         return (idx + 1), loss_val, opt_state
     
@@ -81,13 +113,14 @@ def main(cfg):
     run_loss = 0.0
     run_cnt = 0
     while True:
+        prio_weight = min(1.0, proc_epoch / burnin_epoch)
         x, y = train.sample()
-        idx, loss_val, opt_state = update(idx, opt_state, x, y)
+        idx, loss_val, opt_state = update(idx, opt_state, x, y, prio_weight)
         run_loss += loss_val
         run_cnt += 1
         proc_epoch += (batch_size / 60000)
         t1 = time.time()
-        if (t1 - t0 > log_sec) and (run_cnt > 0):
+        if ((t1 - t0 > log_sec) or (proc_epoch > epoch_num)) and (run_cnt > 0):
             x, y = test.sample(get_all = True)
             log_txt = ""
             for t, txt in enumerate(["epoch={:.2f}".format(proc_epoch),
@@ -99,6 +132,8 @@ def main(cfg):
                     log_txt += ","
                 log_txt += txt
             pickle.dump(get_params(opt_state), open(weight_name, "wb"))
+            with open("log.txt", "a") as f:
+                f.write("{}\n".format(log_txt))
             print(log_txt)
             run_loss = 0.0
             run_cnt = 0
